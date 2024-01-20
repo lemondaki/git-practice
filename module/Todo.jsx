6196d693ef229216087a748e8ec2b5130d0915f3
Hai Ngo <haingo@trudi.ai>
2024-01-20 16:45:41 +0700

    learn git command

---
+++ module/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+      <>
+          <h1>Todo heading</h1>
+          <h2>learn commit and push git command</h2>
+          <h2>learn commit --m amend command</h2>
+      </>
+  )
+}
+
+export default Todo
