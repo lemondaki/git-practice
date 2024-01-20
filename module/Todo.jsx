72fd84f911efa004cab065155c30cb85756ddce8
Hai Ngo <haingo@trudi.ai>
2024-01-20 16:45:41 +0700

    command: learn commit and push

---
+++ module/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+      <>
+          <h1>Todo heading</h1>
+          <h2>learn commit and push git command</h2>
+      </>
+  )
+}
+
+export default Todo
